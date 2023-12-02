9d69e6e742a020de5788f9199f4526bac5aaf544
LeeYunSung <dldbstjd6350@naver.com>
2023-12-02 19:21:17 +0900

    [add] 앨범에서 Profile 사진 추가하기 기능

--- iOS/Pramisu/View/SignUpViewController.swift
+++ iOS/Pramisu/View/SignUpViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var userId: String?
+    @IBOutlet weak var profileImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("UserId: \(String(describing: userId))")
+        // print("UserId: \(String(describing: userId))")
+        
+        let tapImageViewRecognizer = UITapGestureRecognizer(target: self, action: #selector(profileUpload(tapGestureRecognizer:)))
+        profileImage.isUserInteractionEnabled = true
+        profileImage.addGestureRecognizer(tapImageViewRecognizer)
+    }
+    
+    @objc func profileUpload(tapGestureRecognizer: UITapGestureRecognizer){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: false){ () in
+            let alert = UIAlertController(title: "", message: "이미지 선택을 취소하였습니다", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: false)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: false){ () in
+            let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            self.profileImage.image = image
+        }
+    }
+    
+    @IBAction func signUp(_ sender: UIButton) {
+        
     }
 }
